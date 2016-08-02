03ade2a114040422ad318b4a3cc8cef9ab68cbfa
Özgün <OOE@OOE-MacBook-Air.local>
2016-08-02 11:01:06 +0300

    update lib class
    insert notification function

--- swiftlib/SwiftLib.swift
+++ swiftlib/SwiftLib.swift
@@ -8,22 +8,50 @@
 
 import  UIKit
 
+import CWStatusBarNotification
+
 
 //:MARK:- Extension UIViewController
 extension UIViewController{
     func navigationBackGroundColor(backGColor:UIColor,tintC:UIColor){
-        
+
         self.navigationController!.navigationBar.tintColor = tintC
         self.navigationController!.navigationBar.barTintColor = backGColor
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
-        
-        
+
+
+    }
+    //CWStatusBarNotification 
+    func notificationNavigationBar(txt:String,backColor:UIColor){
+
+        let notif = CWStatusBarNotification()
+        notif.notificationStyle = CWNotificationStyle.NavigationBarNotification
+        notif.notificationLabelBackgroundColor = backColor
+        notif.notificationLabelTextColor = UIColor.whiteColor()
+        notif.notificationAnimationOutStyle = CWNotificationAnimationStyle.Top
+        notif.displayNotificationWithMessage(txt, forDuration: 2.5)
+        notif.notificationTappedBlock = {
+            notif.dismissNotification()
+        }
+    }
+
+    func notificationStatusBar(txt:String,backColor:UIColor){
+
+        let notif = CWStatusBarNotification()
+        notif.notificationStyle = CWNotificationStyle.StatusBarNotification
+        notif.notificationLabelBackgroundColor = backColor
+        notif.notificationLabelTextColor = UIColor.whiteColor()
+        notif.notificationAnimationOutStyle = CWNotificationAnimationStyle.Top
+        notif.displayNotificationWithMessage(txt, forDuration: 2.5)
+        notif.notificationTappedBlock = {
+            notif.dismissNotification()
+        }
     }
 }
 
 //:MARK:- Extension ImageView
 extension UIImageView{
-    
+
     class func navigationImageView(name:String)->UIImageView{
         let imageV = UIImageView()
         imageV.frame = CGRect.navigationBarTitleViewHeight()
@@ -33,51 +61,51 @@ extension UIImageView{
         return imageV
     }
     func setRadius(cornerRadius:CGFloat,clipsToBounds:Bool,borderWidth:CGFloat,borderColor:UIColor) -> UIImageView{
-        
+
         self.layer.cornerRadius = cornerRadius
         self.clipsToBounds = clipsToBounds
         self.layer.borderWidth = borderWidth
         self.layer.borderColor = borderColor.CGColor
-        
+
         return self
-        
+
     }
-    
+
 }
 //:MARK:- Extension CGRect
 extension CGRect{
-    
+
     static func navigationBarTitleViewHeight()->CGRect{
-        
+
         return CGRect(x: 0, y: 0, width: 82.5, height: 33)
     }
 
 }
 //:MARK:- Extension Image
 extension UIImage {
-    
+
     func tintImageColor(color: UIColor) -> UIImage {
-        
+
         UIGraphicsBeginImageContextWithOptions(self.size, false, UIScreen.mainScreen().scale)
         let context :CGContextRef! = UIGraphicsGetCurrentContext()
-        
+
         CGContextTranslateCTM(context, 0, self.size.height)
         CGContextScaleCTM(context, 1.0, -1.0)
-        
+
         let rect:CGRect = CGRectMake(0, 0, self.size.width, self.size.height)
-        
+
         CGContextSetBlendMode(context, CGBlendMode.Normal)
         CGContextDrawImage(context, rect, self.CGImage)
-        
+
         CGContextSetBlendMode(context, CGBlendMode.SourceIn)
         color.setFill()
         CGContextFillRect(context, rect)
-        
+
         let coloredImage:UIImage! = UIGraphicsGetImageFromCurrentImageContext().imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
         UIGraphicsEndImageContext()
-        
+
         return coloredImage
-        
+
     }
 }
 
@@ -114,34 +142,34 @@ extension Int{
 
 //:MARK:- Functions
 func isValidEmail(testStr:String) -> Bool {
-    
+
     let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
     let range = testStr.rangeOfString(emailRegEx, options:.RegularExpressionSearch)
     let result = range != nil ? true : false
     return result
-    
+
 }
 func getWaitProcces(style : UIActivityIndicatorViewStyle)->UIView {
     let load = UIActivityIndicatorView(activityIndicatorStyle: style)
     load.frame = CGRectMake(0, 0, 20, 20)
     load.startAnimating()
-    
+
     return load
 }
 func isValidPhone(value: String) -> Bool {
-    
+
     let charcter  = NSCharacterSet(charactersInString: "555450123456789").invertedSet
     var filtered:NSString!
     let inputString:NSArray = value.componentsSeparatedByCharactersInSet(charcter)
     filtered = inputString.componentsJoinedByString("")
     return  value == filtered
-    
+
 }
 func localized(string:String) -> String {
     return NSLocalizedString(string,comment:"")
 }
 func newImageSize(image:UIImage,scaledToSize newSize:CGSize)->UIImage{
-    
+
     UIGraphicsBeginImageContext( newSize )
     image.drawInRect(CGRect(x: 0,y: 0,width: newSize.width,height: newSize.height))
     let newImage = UIGraphicsGetImageFromCurrentImageContext()
@@ -149,35 +177,35 @@ func newImageSize(image:UIImage,scaledToSize newSize:CGSize)->UIImage{
     return newImage.imageWithRenderingMode(.AlwaysTemplate)
 }
 func heightToText(text:String, font:UIFont, width:CGFloat) -> CGFloat{
-    
+
     let label:UILabel = UILabel(frame: CGRectMake(0, 0, width, CGFloat.max))
     label.numberOfLines = 0
     label.lineBreakMode = NSLineBreakMode.ByWordWrapping
     label.font = font
     label.text = text
-    
+
     label.sizeToFit()
     return label.frame.height
 }
 func widthToText(text:String, font:UIFont) -> CGFloat{
-    
+
     let label:UILabel = UILabel(frame: CGRectMake(0, 0, CGFloat.max, CGFloat.max))
     label.numberOfLines = 0
     label.lineBreakMode = NSLineBreakMode.ByWordWrapping
     label.font = font
     label.text = text
-    
+
     label.sizeToFit()
     return label.frame.width
 }
 func getRandomColor() -> UIColor{
-    
+
     let randomRed:CGFloat = CGFloat(drand48())
-    
+
     let randomGreen:CGFloat = CGFloat(drand48())
-    
+
     let randomBlue:CGFloat = CGFloat(drand48())
-    
+
     return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     
 }
